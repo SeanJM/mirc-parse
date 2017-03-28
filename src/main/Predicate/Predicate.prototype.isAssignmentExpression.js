c6db08854bc79f781653b377576c893ed1069bf0
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-03-28 09:14:33 -0400

    fixed missing 'source' property

--- src/main/Predicate/Predicate.prototype.isAssignmentExpression.js
+++ src/main/Predicate/Predicate.prototype.isAssignmentExpression.js
@@ -1,4 +1,4 @@
 Predicate.prototype.isAssignmentExpression = function () {
   var slice = this.string.substring(this.start, this.end);
-  return new RegExp(EXP_VAR + '(\s+|)=').test(slice);
+  return new RegExp(EXP_VAR.source + '(\\s+|)=').test(slice);
 };
