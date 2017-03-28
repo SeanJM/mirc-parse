de8c15ebef952f0205c1e2ce9d69bd8c8754e4b6
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-03-28 09:14:49 -0400

    fixed method formatting

--- src/main/Statement/Statement.js
+++ src/main/Statement/Statement.js
@@ -3,22 +3,22 @@ function Statement(opt) {
   Object.assign(this, opt);
 
   if (/^(\/|)var\b/.test(slice)) {
-    return this['variableDeclaration']();
+    return this.variableDeclaration();
   } else if (/^%[a-zA-Z\-\_0-9]+(\s+)=/.test(slice)) {
-    return this['assignmentStatement']();
+    return this.assignmentStatement();
   } else if (/^(\/|)alias\b/.test(slice)) {
-    return this['functionDeclaration']();
+    return this.functionDeclaration();
   } else if (/^(\/|)[a-zA-Z0-9\-\_]+/.test(slice)) {
-    return this['functionStatement']();
+    return this.functionStatement();
   } else if (/^\$[a-zA-Z0-9\-\_]+/.test(slice)) {
-    return this['functionStatement']();
+    return this.functionStatement();
   } else if (/^\%[\:\.a-zA-Z0-9\-\_]+/.test(slice)) {
-    return this['functionStatement']();
+    return this.functionStatement();
   } else if (/^(\/|)return\b/.test(slice)) {
-    return this['returnStatement']();
+    return this.returnStatement();
   } else if (/^(\/|)halt\b/.test(slice)) {
-    return this['haltStatement']();
+    return this.haltStatement();
   } else if (/^if\b/.test(slice)) {
-    return this['ifStatement']();
+    return this.ifStatement();
   }
 }
