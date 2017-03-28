d67eb3d73cbc0f3d487960eb60ad37f391461157
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-03-28 09:28:53 -0400

    added test for assignment statement

---
+++ test/tests/assignment_statement.js
@@ -0,0 +1,45 @@
+const ParseMirc = require('../../mirc-parse.js');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  name : 'Assignment statement',
+  this : function () {
+    const str = fs.readFileSync(path.resolve('test/mrc/assignment_statement.mrc'), 'utf8');
+    const parser = new ParseMirc(str);
+    const result = parser.parse(str);
+    console.log(JSON.stringify(result, null, '  '));
+    return result;
+  },
+  isDeepEqual : function () {
+    return {
+      "type": "mirc",
+      "start": 0,
+      "end": 15,
+      "body": [
+        {
+          "type": "assignmentStatement",
+          "start": 0,
+          "end": 15,
+          "expression": {
+            "type": "assignmentExpression",
+            "operator": "=",
+            "left": {
+              "type": "identifier",
+              "start": 0,
+              "name": "%identifier",
+              "end": 11
+            },
+            "right": {
+              "type": "literal",
+              "value": "5",
+              "raw": "5",
+              "start": 14,
+              "end": 15
+            }
+          }
+        }
+      ]
+    };
+  }
+};
