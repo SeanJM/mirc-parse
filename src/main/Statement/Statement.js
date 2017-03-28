186b5f75f8603320dc18f4cfff7231e70a9f1327
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-03-28 10:02:24 -0400

    added 'this.block()' to 'Statements'

--- src/main/Statement/Statement.js
+++ src/main/Statement/Statement.js
@@ -8,17 +8,19 @@ function Statement(opt) {
     return this.assignmentStatement();
   } else if (/^(\/|)alias\b/.test(slice)) {
     return this.functionDeclaration();
-  } else if (/^(\/|)[a-zA-Z0-9\-\_]+/.test(slice)) {
-    return this.functionStatement();
-  } else if (/^\$[a-zA-Z0-9\-\_]+/.test(slice)) {
-    return this.functionStatement();
-  } else if (/^\%[\:\.a-zA-Z0-9\-\_]+/.test(slice)) {
-    return this.functionStatement();
   } else if (/^(\/|)return\b/.test(slice)) {
     return this.returnStatement();
   } else if (/^(\/|)halt\b/.test(slice)) {
     return this.haltStatement();
   } else if (/^if\b/.test(slice)) {
     return this.ifStatement();
+  } else if (slice[0] === '{') {
+    return this.block();
+  } else if (/^(\/|)[a-zA-Z0-9\-\_]+/.test(slice)) {
+    return this.functionStatement();
+  } else if (/^\$[a-zA-Z0-9\-\_]+/.test(slice)) {
+    return this.functionStatement();
+  } else if (/^\%[\:\.a-zA-Z0-9\-\_]+/.test(slice)) {
+    return this.functionStatement();
   }
 }
