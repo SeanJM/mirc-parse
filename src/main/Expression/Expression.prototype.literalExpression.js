94440f2a00f7ac0a1fb659b2d2db6becad96daa5
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-04-01 12:05:00 -0400

    literalExpression will capture till it encounters a non literal

--- src/main/Expression/Expression.prototype.literalExpression.js
+++ src/main/Expression/Expression.prototype.literalExpression.js
@@ -1,12 +1,26 @@
 Expression.prototype.literalExpression = function () {
-  var value = this.string.substring(this.start, this.end);
-  var isNumber = /^[0-9]+(\.[0-9]+|)$/.test(value);
+  var i = this.start;
+  var n = this.end;
+  var string = this.string;
+
+  var value;
+  var isNumber;
+
+  while (i < n && !/^\s(%|\$)/.test(string.substring(i, i + 3))) {
+    i += 1;
+  }
+
+  value = string
+    .substring(this.start, i)
+    .replace(/\r\n|\n/g, '');
+
+  isNumber = /^[0-9]+(\.[0-9]+|)$/.test(value);
 
   return {
     type : 'literal',
     value : value,
     raw : isNumber ? value : '\'' + value + '\'',
     start : this.start,
-    end : this.end
+    end : i
   };
 };
